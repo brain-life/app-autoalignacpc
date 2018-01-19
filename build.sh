d14fa1b43aba0981471f5f99fb8d4f08e897fb89
francopestilli <frakkopesto@gmail.com>
2018-01-19 15:33:29 -0500

    ENH? Updating the app to newer version by removing start/stop/status and adding a dockerversion

--- build.sh
+++ build.sh
@@ -1,1 +1,10 @@
-docker build -t brainlife/alignacpc .
+#!/bin/bash
+module load matlab/2017a
+cat > build.m <<END
+addpath(genpath('/N/u/brlife/git/vistasoft'))
+addpath(genpath('/N/u/brlife/git/jsonlab'))
+addpath(genpath('/N/soft/mason/SPM/spm8'))
+mcc -m -R -nodisplay -d compiled autoalign-matlab
+exit
+END
+matlab -nodisplay -nosplash -r build
