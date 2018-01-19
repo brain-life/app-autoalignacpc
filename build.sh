333e71e1296d7b74d0c37bb67d2d17861e08aa48
francopestilli <frakkopesto@gmail.com>
2018-01-19 18:41:40 -0500

    Docker

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ cat > build.m <<END
 addpath(genpath('/N/u/brlife/git/vistasoft'))
 addpath(genpath('/N/u/brlife/git/jsonlab'))
 addpath(genpath('/N/soft/mason/SPM/spm8'))
-mcc -m -R -nodisplay -d compiled autoalign-matlab
+mcc -m -R -nodisplay -d compiled autoalignmatlab
 exit
 END
 matlab -nodisplay -nosplash -r build
