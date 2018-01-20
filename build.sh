b2eab59ac86c0914b2cc08a98f9c169ad60f6707
francopestilli <frakkopesto@gmail.com>
2018-01-20 16:28:09 -0500

    Using a different VISTASOFT path

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 module load matlab/2017a
 cat > build.m <<END
-addpath(genpath('/N/u/brlife/git/vistasoft'))
+addpath(genpath('/N/u/hayashis/BigRed2/git/vistasoft'))
 addpath(genpath('/N/u/brlife/git/jsonlab'))
 addpath(genpath('/N/soft/mason/SPM/spm8'))
 mcc -m -R -nodisplay -d compiled autoalignmatlab
